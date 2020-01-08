308df1807881ca23f4585547f060903885822119
Shivaram Mahapatro <shivaram@ntspl.co.in>
2020-01-09 02:10:31 +0530

    Initial Commit

---
+++ Backend/config.php
@@ -0,0 +1,20 @@
+<?php
+
+class cleanto_myvariable {
+	/*
+    public $hostnames = "mariadb-001.wc1.phx1.stabletransit.com";
+    public $username = "377900_yupserv";
+    public $passwords = "Y45@!@!ups!@!erv";
+    public $database = "377900_yupserv";
+    public $epcode = "994a138b-93be-42c8-bb2c-ae3f0196f392";
+	*/
+	
+	public $hostnames = "mariadb-001.wc1.phx1.stabletransit.com";
+    public $username = "377900_yupserv";
+    public $passwords = "Y45@!@!ups!@!erv";
+    public $database = "377900_yupserv";
+    public $epcode = "994a138b-93be-42c8-bb2c-ae3f0196f392";
+	
+}
+
+?>
